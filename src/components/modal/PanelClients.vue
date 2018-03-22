5b7107dcdf1564005bcb16d8bd5d2072404c27cc
Michaela Lehr <michaela.lehr@geildanke.com>
2018-03-22 15:48:11 +0100

    MOD style clients panel tabs

--- src/components/modal/PanelClients.vue
+++ src/components/modal/PanelClients.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="clients">
-    <div class="">
-      <button class="">App<span class=""></span></button><!--
-      --><button class="">Cloud<span class=""></span></button>
+    <div class="clients__tabs">
+      <button class="clients__tab" :class="{ 'clients__tab--active': activeTab === 0 }" :style="activeTabStyle(0)" v-on:click="handleTabClick()">App<span class="clients__tab__indicator" :style="activeIndicatorStyle(0)"></span></button><!--
+      --><button class="clients__tab" :class="{ 'clients__tab--active': activeTab === 1 }" :style="activeTabStyle(1)" v-on:click="handleTabClick()">Cloud<span class="clients__tab__indicator" :style="activeIndicatorStyle(1)"></span></button>
     </div>
     <div>
       <ul>
@@ -22,6 +22,7 @@ export default {
   data() {
     return {
       hoverState: false,
+      activeTab: 0,
       show: this.$select('show'),
       clients: [
         {
@@ -70,9 +71,37 @@ export default {
     handleNextClick() {
       store.dispatch(store.actions.panelFinish());
     },
+    handleTabClick() {
+      if (this.activeTab === 0) {
+        this.activeTab = 1;
+      } else {
+        this.activeTab = 0;
+      }
+    },
     updateHoverState(isHover) {
       console.log(isHover)
       this.hoverState = isHover;
+    },
+    activeTabStyle(index) {
+      let color = 'inherit';
+
+      if (index === this.activeTab) {
+        color = this.theme.unthemedButton['backgroundColor'];
+      }
+      return {
+        color: color
+      }
+    },
+    activeIndicatorStyle(index) {
+      let backgroundColor = 'inherit';
+
+      if (index === this.activeTab) {
+        backgroundColor = this.theme.unthemedButton['backgroundColor'];
+      }
+
+      return {
+        backgroundColor: backgroundColor
+      }
     }
   }
 }
@@ -86,10 +115,50 @@ export default {
     font-size: 14px;
     text-align: center;
 
-    padding: 64px 20px 40px 20px;
+    padding: 48px 0 0 0;
     position: absolute;
     width: 300px;
     height: 100%;
+  }
+  .clients__tabs {
+    background-color: #F2F2F2;
+    border-bottom: 1px solid #CBCBCB;
+    border-top: 1px solid #E1E1E1;
+    padding: 0 20px;
+    height: 48px;
+  }
+  .clients__tab {
+    background-color: transparent;
+    border: none;
+    color: #AAAAAA;
+    cursor: pointer;
+    display: inline-block;
+    font-size: 16px !important;
+    font-weight: 300;
+    height: 100%;
+    line-height: 1em;
+    margin: 0;
+    padding: 16px 0 14px 0;
+    outline: none;
+    width: 50%;
+  }
+  .clients__tab--active {
+    position: relative;
+
+    .clients__tab__indicator {
+      transform: scaleX(1);
+    }
+  }
+  .clients__tab__indicator {
+    bottom: -1.5px;
+    display: block;
+    height: 3px;
+    left: 20px;
+    position: absolute;
+    right: 20px;
+    transform-origin: 50% 50%;
+    transition: transform 0.25s cubic-bezier(0.62, 0.28, 0.23, 0.99);
+    width: 90px;
   }
   .finish__button {
     border-radius: 1px;
